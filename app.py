b320294c2f50716142028b022d2af2562d96e557
Sally Huang <sallyhuang@Sallys-iMac.local>
2023-11-22 19:34:28 -0600

    updated app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
 df = pd.read_csv("file:///Users/sallyhuang/SDTProject/vehicles_us.csv")
 
